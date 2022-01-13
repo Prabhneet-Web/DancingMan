e98be91ae0ddc12bfe90a2a0d6c89020b68e4ee8
Prabhneet Narula <imailcode@gmail.com>
2022-01-13 20:25:41 +0530

    Some Other Changes Done.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayer
+import audioplayers
+import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayerPlugin.register(with: registry.registrar(forPlugin: "AudioplayerPlugin"))
+  AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
